3c676d11d32af9cf5d998d9324626bb79355441d
dkarmazi <dkarmazi@gwu.edu>
2014-02-28 20:53:44 -0500

    RMI working

--- src/org/certus/rmi/ServerInterface.java
+++
@@ -1,9 +0,0 @@
-package org.certus.rmi;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ServerInterface extends Remote {
-    String sayHello(String name) throws RemoteException;
-    
-}
